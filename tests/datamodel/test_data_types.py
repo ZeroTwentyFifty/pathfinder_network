6298a5c351c0f47543e5217a1af38e3ff02dacf5
JohnVonNeumann <18162779+JohnVonNeumann@users.noreply.github.com>
2023-03-09 11:26:28 +1100

    UPDATE test_dt.py - add test_pfid_is_uuid4

--- tests/datamodel/test_data_types.py
+++ tests/datamodel/test_data_types.py
@@ -1,11 +1,18 @@
 import json
+import uuid
 
 from pathfinder_network.datamodel.data_types import PfId
 
 
-def test_pfid_is_uuid4():
+def test_pfid_json_representation():
     pfid: PfId = PfId()
 
     json_dict = json.dumps(dict(id=str(pfid.id)))
 
     assert pfid.json() == json_dict
+
+
+def test_pfid_is_uuid4():
+    pfid: PfId = PfId()
+
+    assert uuid.UUID(hex=str(pfid.id), version=4)
