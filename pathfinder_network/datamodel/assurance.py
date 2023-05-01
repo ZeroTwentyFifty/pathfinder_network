ebf1297b874998dcf66daa31e0350edb2eeb3221
JohnVonNeumann <18162779+JohnVonNeumann@users.noreply.github.com>
2023-05-01 22:29:08 +1000

    UPDATE assurance.py - add optional attributes

--- pathfinder_network/datamodel/assurance.py
+++ pathfinder_network/datamodel/assurance.py
@@ -30,6 +30,33 @@ class Assurance(BaseModel):
     Attributes:
     - assurance (bool): Binary indicator stating whether the PCF has been assured in line with
     Pathfinder Framework requirements
+    - coverage (str): Level of granularity of the emissions data assured. Valid values are:
+        - "corporate level" for corporate level
+        - "product line" for product line
+        - "PCF system" for PCF System
+        - "product level" for product level
+        This property MAY be undefined only if the kind of assurance was not performed.
+    - level (str): Level of assurance applicable to the PCF. Valid values are:
+        - "limited" for limited assurance
+        - "reasonable" for reasonable assurance
+        This property is optional.
+    - boundary (str): Boundary of the assurance. Valid values are:
+        - "Gate-to-Gate" for Gate-to-Gate
+        - "Cradle-to-Gate" for Cradle-to-Gate
+        This property is optional.
+    - providerName (str): The non-empty name of the independent third party engaged to undertake the assurance.
+        This property is required.
+    - completedAt (datetime): The date at which the assurance was completed. This property is optional.
+    - standardName (str): Name of the standard against which the PCF was assured. This property is optional.
+    - comments (str): Any additional comments that will clarify the interpretation of the assurance.
+        This property may be an empty string. This property is optional.
     """
 
     assurance: bool
+    coverage: AssuranceCoverage | None = None
+    level: AssuranceLevel | None = None
+    boundary: AssuranceBoundary | None = None
+    providerName: NonEmptyString | None = None
+    completedAt: datetime | None = None
+    standardName: str | None = None
+    comments: str | None = None
