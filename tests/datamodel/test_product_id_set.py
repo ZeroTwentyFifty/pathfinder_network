c1a9aff3f8e5de2670aab8ff0f4a2807a6519b24
JohnVonNeumann <18162779+JohnVonNeumann@users.noreply.github.com>
2023-05-09 21:01:24 +1000

    UPDATE product_id_set.py - add __eq__ special method and tests

--- tests/datamodel/test_product_id_set.py
+++ tests/datamodel/test_product_id_set.py
@@ -4,7 +4,17 @@ from pathfinder_network.datamodel.product_id import ProductId
 from pathfinder_network.datamodel.product_id_set import ProductIdSet
 
 
-def test_product_id_set_with_valid_ids():
+@pytest.fixture
+def valid_product_id_string_list():
+    ids = [
+        "urn:pathfinder:product:customcode:buyer-assigned:4321",
+        "urn:pathfinder:product:customcode:vendor-assigned:6789",
+    ]
+    return ids
+
+
+@pytest.fixture
+def valid_product_id_list():
     ids = [
         ProductId(value="urn:foo:params:pid:product:customcode:buyer-assigned:foo-bar"),
         ProductId(value="urn:foo:params:pid:product:id:cas:1234-56-8"),
@@ -12,8 +22,17 @@ def test_product_id_set_with_valid_ids():
             value="urn:foo:params:pid:product:id:iupac-inchi:InChI=1S/C6H12O6/c7-1-2-3(8)4(9)5(10)6(11)12-2/h2-11H,1H2/t2-,3-,4+,5-,6?/m1/s1"
         ),
     ]
-    product_id_set = ProductIdSet(product_ids=ids)
-    assert product_id_set.product_ids == ids
+    return ids
+
+
+@pytest.fixture
+def valid_product_id_set(valid_product_id_list):
+    product_id_set = ProductIdSet(product_ids=valid_product_id_list)
+    return product_id_set
+
+
+def test_product_id_set_with_valid_ids(valid_product_id_set, valid_product_id_list):
+    assert valid_product_id_set.product_ids == valid_product_id_list
 
 
 def test_product_id_set_with_valid_id_strings():
@@ -24,6 +43,21 @@ def test_product_id_set_with_valid_id_strings():
     ]
     product_id_set = ProductIdSet(product_ids=ids)
     assert str(product_id_set) == f"[{', '.join(map(str, ids))}]"
+
+
+def test_product_id_set_comparison(valid_product_id_set, valid_product_id_list):
+    product_id_set = ProductIdSet(product_ids=valid_product_id_list)
+    assert product_id_set == valid_product_id_set
+
+
+def test_product_id_set_string_list_comparison(
+    valid_product_id_list, valid_product_id_string_list
+):
+    product_id_set = ProductIdSet(product_ids=valid_product_id_string_list)
+    assert product_id_set == [
+        "urn:pathfinder:product:customcode:buyer-assigned:4321",
+        "urn:pathfinder:product:customcode:vendor-assigned:6789",
+    ]
 
 
 def test_product_id_set_with_empty_list():
